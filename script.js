3d11b52081f053d7ed90575a7967af211b40f4c5
Sandesh <112424645+Sandy177000@users.noreply.github.com>
2024-01-23 21:36:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+var element = document.getElementById("time");
+
+setInterval(function () {
+	var d = new Date();
+	element.innerHTML = d.toLocaleString();
+},1000)
+
+
+
+
+
+
